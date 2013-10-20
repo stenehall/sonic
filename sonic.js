fcb72758baf3356fa601598e64ac545468ead5e6
Johan Stenehall <stenehall@gmail.com>
2013-10-20 23:44:26 +0200

    Adding loading of npm plugins

--- sonic.js
+++ sonic.js
@@ -1,4 +1,25 @@
 var Igelkott = require('igelkott');
 
-var sonic = new Igelkott;
+var Sonic = function Sonic() {
+  Igelkott.call(this);
+};
+
+Sonic.prototype = Object.create(Igelkott.prototype, {constructor: {value: Sonic}});
+
+Sonic.prototype.load = function load(pluginName) {
+  try {
+    Igelkott.prototype.load.call(this, pluginName);
+  }
+  catch (err) {
+    this.log(err)
+    try {
+      this.plugin.load(pluginName, require('igelkott-'+pluginName).Plugin);
+    } catch (err)
+    {
+      this.log('No such plugin: '+pluginName);
+    }
+  }
+};
+
+var sonic = new Sonic();
 sonic.connect();
